8d7abe326c259b25fd2ab1ca08b05c81a494bbff
vitosg90 <ludikiv7@gmail.com>
2026-04-24 10:50:59 +0300

    Update MiniSlotsClient.java

--- src/main/java/com/example/minislots/client/MiniSlotsClient.java
+++ src/main/java/com/example/minislots/client/MiniSlotsClient.java
@@ -4,6 +4,7 @@ import com.example.minislots.client.screen.MiniSlotsScreen;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.text.Text;
 
 public class MiniSlotsClient implements ClientModInitializer {
@@ -12,8 +13,9 @@ public class MiniSlotsClient implements ClientModInitializer {
         ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) ->
                 dispatcher.register(ClientCommandManager.literal("minislots")
                         .executes(context -> {
-                            if (context.getSource().getClient() != null) {
-                                context.getSource().getClient().setScreen(new MiniSlotsScreen());
+                            MinecraftClient client = context.getSource().getClient();
+                            if (client != null) {
+                                client.execute(() -> client.setScreen(new MiniSlotsScreen()));
                                 context.getSource().sendFeedback(Text.literal("Opened Mini Slots UI"));
                             }
                             return 1;
